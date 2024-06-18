a116889179800f09d924c8cefef294fe6ea16fce
L3ktor <goopapota@gmail.com>
2024-06-18 16:35:44 +0300

    Integration quiz

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -2,28 +2,34 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.service.UserService;
-
-
 
 @RestController
-@RequestMapping("/api/user")
 public class UserController {
-    private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable long id) {
-        final User user = userService.getUserById(id);
-        return user != null
-                ? new ResponseEntity<>(user, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/user-data")
+    public ResponseEntity<User> showUser(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
+//если вы просите меня удалить данный код, то пожалуйста, прочтите сообщение,
+// в котором я указал логику действий
+
+//AuthController
+//Он обрабатывает GET-запросы к эндпоинту /user-data,
+//извлекает объект User, представляющий текущего вошедшего в систему пользователя,
+//из объекта Authentication и возвращает его в виде ответа HTTP с кодом состояния 200 (OK).
+
+//Authorize.js
+//Выполняет асинхронный запрос fetch к эндпоинту /user-data
+// для получения данных о пользователе в формате JSON.
+// Затем он обрабатывает полученные данные и обновляет
+// пользовательский интерфейс в соответствии с ними.
+
+
 
